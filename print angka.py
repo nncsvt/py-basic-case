cc0d4e2de9722af160b43cc7f062960a11ccc14c
nncsvt <91187321+nncsvt@users.noreply.github.com>
2023-02-12 17:22:46 +0700

    Add files via upload

---
+++ print angka.py
@@ -0,0 +1,8 @@
+#### TUlis sebuah program yang dapat memunculkan angka yang dimasukkan user
+## masukan berupa integer
+
+angka = int(input('masukkan angka : '))
+
+print("angka yang dimasukkan : " + str(angka))
+print("angka yang dimasukkan : ",angka)
+##perbedaan pakai danda + dane (,)
